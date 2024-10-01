d639186753691d2e4724fae30ed1d6df363fc197
asiancart <asiancart@hotmail.com>
2024-10-01 16:18:50 +0300

    Add hangman project

---
+++ hangman/Program.cs
@@ -0,0 +1,48 @@
+﻿string word = "hello";
+
+int maxLives = 7;
+int currentLives = maxLives;
+
+bool win = false;
+
+List<char> guessedLetters = new List<char>();
+
+
+while (currentLives>0 && !win)
+{
+    foreach(char c in word)
+    {
+        if (guessedLetters.Contains(c))
+            Console.Write(c);
+        else
+            Console.Write("_");
+    }
+    Console.WriteLine("\nPlease guess a letter!");
+    Console.WriteLine(currentLives + "/" +maxLives + " lives remaining. ");
+
+    char guess = Convert.ToChar(Console.ReadLine());
+
+    if(word.Contains(guess) && !guessedLetters.Contains(guess))
+    {
+        Console.WriteLine("Correct!");
+    }
+    else
+    {
+        Console.WriteLine("Incorrect!");
+        currentLives--;
+    }
+    guessedLetters.Add(guess);
+    
+    bool worldComplete= true;
+
+    foreach(char c in word)
+        if(!guessedLetters.Contains(c))
+            worldComplete= false;
+
+    win = worldComplete;
+}
+
+if(win)
+    Console.WriteLine("Congrulations, you win");
+else
+    Console.WriteLine("You lose.");
